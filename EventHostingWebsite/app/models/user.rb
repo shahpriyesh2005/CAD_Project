3532975636e0dd2e4df1ee16987bc14b8ab80daa
Priyesh Shah <55635249+shahpriyesh2005@users.noreply.github.com>
2019-10-25 02:10:16 +0100

    Added form validations in model files
    Added form validations in user, event, ticket, order and rating model files.

--- EventHostingWebsite/app/models/user.rb
+++ EventHostingWebsite/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :events
     has_many :subscriptions
+
+    validates_format_of :first_name, :last_name, :gender, :address1, :city, :county, :country, :with => /[a-z]+/i, :presence => true
+    validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :presence => true
+    validates_format_of :contact_no, :with => /[0-9]+/i, :presence => true
 end
