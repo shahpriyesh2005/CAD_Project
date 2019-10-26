aeeec648100d381fb1fb55df6ffaa83453b11c2f
Ghiridhar <ghiridhar2712@gmail.com>
2019-10-26 10:21:37 +0100

    Update routes.rb
    Set dependency of all scaffolds on User

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users do # Only if there is an user logged in he/she can subscribe, wishlist or add event
+  resources :users do # Only if there is an user logged in he/she can work
     resources :subscriptions
     resources :wishlists
-    resources :events do # Only if there is an event you can  book tickets
-      resources :tickets
-    end
+    resources :events 
+    resources :tickets
+    resources :orders
+    resources :ratings
   end
-  resources :orders
-  resources :ratings
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
