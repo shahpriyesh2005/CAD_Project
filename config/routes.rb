2745b6a0045a44a1850591cee3e0cf151246dcc3
Priyesh Shah <55635249+shahpriyesh2005@users.noreply.github.com>
2019-10-29 16:55:32 +0000

    Implemented devise login, changed routes, added bootstrap for GUI, modified users scaffold to handle login
    Implemented devise login for the application, added bootstrap for making GUI attractive, modified users scaffold to handle login. Also changed routes to remove complications related to user and nested resources dependency.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  resources :users do # Only if there is an user logged in he/she can subscribe, wishlist or add event
-    resources :subscriptions
-    resources :wishlists
-    resources :events # Only if there is an event you can  book tickets
+  devise_for :users
+  root 'home#index'
+  resources :users
+  resources :orders
+  resources :ratings
+  resources :wishlists
+  resources :subscriptions
+
+  resources :events do
     resources :tickets
-    resources :orders
-    resources :ratings
   end
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
