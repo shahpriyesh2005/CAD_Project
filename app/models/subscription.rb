1631e2c951d6d793515571a4434b30aba1650da1
Priyesh Shah <55635249+shahpriyesh2005@users.noreply.github.com>
2019-11-03 19:24:59 +0000

    Small change

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,7 +3,10 @@ class Subscription < ApplicationRecord
 
   USER_NAMES = []
   @users = User.find_by_sql("select users.first_name,users.last_name from users join events on users.id=events.user_id")
-  @users.each do |name|
-    USER_NAMES << name.first_name.concat(" ").concat(name.last_name)
+
+  unless @users.nil?
+    @users.each do |name|
+      USER_NAMES << name.first_name.concat(" ").concat(name.last_name)
+    end
   end
 end
