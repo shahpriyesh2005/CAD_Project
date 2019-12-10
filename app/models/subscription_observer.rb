7809d14c28032547d62edd8d15da9a3b609cfe82
Ghiridhar <ghiridhar2712@gmail.com>
2019-12-10 15:55:47 +0000

    Bug Fixes
    Bug Fixes

--- app/models/subscription_observer.rb
+++ app/models/subscription_observer.rb
@@ -3,7 +3,7 @@ class SubscriptionObserver < ActiveRecord::Observer
         @organizer = record.subscribed_user_id
         @current_user = User.find(record.user_id)
         @organizer_name = User.find(@organizer)
-        insert_notify_query = "insert into notifications(users_id,events_id,notify_category,seen,created_at,updated_at) values(#{@organizer},1,'Subscription',false,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)"
+        insert_notify_query = "insert into notifications(users_id,events_id,notify_category,seen,created_at,updated_at) values(#{@organizer},#{Event.last.id},'Subscription',false,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)"
         ActiveRecord::Base.connection.execute(insert_notify_query)           
         
     end
